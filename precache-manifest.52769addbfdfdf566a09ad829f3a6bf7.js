dc4a6f9334bf5bee5c52f295eb97ec474cc2890a
kabeh2 <mattcnk@hotmail.com>
2020-01-17 10:05:46 -0500

    Updates

---
+++ precache-manifest.52769addbfdfdf566a09ad829f3a6bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41291f64639f937fc6b1c62a7d1e35ab",
+    "url": "/Auth-Friends/index.html"
+  },
+  {
+    "revision": "287dc23712af9ce89a83",
+    "url": "/Auth-Friends/static/css/main.107cce36.chunk.css"
+  },
+  {
+    "revision": "7a8b2d7499a858b7a704",
+    "url": "/Auth-Friends/static/js/2.bc933508.chunk.js"
+  },
+  {
+    "revision": "efbfc4d0a025ac6ac763bd4325c2a005",
+    "url": "/Auth-Friends/static/js/2.bc933508.chunk.js.LICENSE"
+  },
+  {
+    "revision": "287dc23712af9ce89a83",
+    "url": "/Auth-Friends/static/js/main.5921bc7f.chunk.js"
+  },
+  {
+    "revision": "f951b030cae356e21d21",
+    "url": "/Auth-Friends/static/js/runtime-main.c6fb2204.js"
+  }
+]);
